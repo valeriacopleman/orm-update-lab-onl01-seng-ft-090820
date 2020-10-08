e6adbba475525dec165272b255c686af56000be0
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 19:31:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
       LIMIT 1
     SQL
  
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
     # find the student in the database given a name
