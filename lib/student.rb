84a94564595b4a3b3b5130b59c12d98fc61b1047
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 19:31:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
  
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-    end.first
+    end
     # find the student in the database given a name
     # return a new instance of the Student class
   end
