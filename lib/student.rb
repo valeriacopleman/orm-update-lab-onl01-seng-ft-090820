51d48ad615d93795ef279b4579dd9830a07f74f9
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 19:19:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,15 @@ class Student
     student.save
     student
   end
+  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+    # create a new Student object given a row from the database
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
